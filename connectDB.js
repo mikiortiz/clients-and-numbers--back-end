8cb94d65e61f9541d2342eaefc08c224adcafc73
Miguel Ortiz <ortizmichel390@gmail.com>
2024-04-09 11:54:20 -0300

    Added new routes for numbers and established connection to the database

---
+++ connectDB.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+module.exports = async function connectDB() {
+  try {
+    mongoose.set("debug", true);
+    await mongoose.connect(
+      "mongodb+srv://ortizmichel390:Larissa2015@cluster0.viztkjm.mongodb.net/Clients-Numbers?retryWrites=true&w=majority"
+    );
+    console.log(">>> DB is connected<<<");
+  } catch (error) {
+    console.log(error);
+  }
+};
